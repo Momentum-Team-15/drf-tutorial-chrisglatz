3fec68104988586cef7c169872a824a36fd44cf6
Chris Glatz <cg.glatz@gmail.com>
2022-11-07 20:17:32 -0500

    something broke, stopping point here

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,7 +3,16 @@ from rest_framework import serializers
 from .models import Snippet
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', )
+        fields = ('id', 'title', 'code', 'linenos', 'owner' )
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
